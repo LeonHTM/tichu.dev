072c037c226f39956994f7e259634358dd9aa825
LeonHTM <leon.gabriel.strauss@icloud.com>
2026-09-08 00:03:12 +0200

    Optimized for mobile

--- tichuFrontServer.py
+++ tichuFrontServer.py
@@ -1,8 +1,18 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, send_from_directory
 
 def create_app():
     app = Flask(__name__, template_folder="routes")
 
+
+    @app.route("/.well-known/apple-app-site-association")
+    def apple_app_site_association():
+        return send_from_directory(
+            "/Users/leon/Desktop/TichuServer/.well-known",
+            "apple-app-site-association",
+            mimetype="application/json"
+        )
+
+    
     @app.route("/", methods=["GET"])
     def browser_dashboard():
             return render_template("main.html")
